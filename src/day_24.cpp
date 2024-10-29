4ac0e906f446311ed0bd79e8a75ebe9a00090f36
emrecan-y <emrecanyilmaz1997@gmail.com>
2024-10-29 14:37:21 +0100

    implement solution for day24

--- src/day_24.cpp
+++ src/day_24.cpp
@@ -10,6 +10,28 @@ struct blizzard
     int y;
 };
 
+struct person_pos_3d
+{
+    person_pos_3d(int x, int y, int maze_index)
+        : x(x), y(y), maze_index(maze_index) {}
+    int x;
+    int y;
+    int maze_index;
+
+    bool operator<(const person_pos_3d &other) const
+    {
+        if (x != other.x)
+        {
+            return x < other.x;
+        }
+        if (y != other.y)
+        {
+            return y < other.y;
+        }
+        return maze_index < other.maze_index;
+    }
+};
+
 struct blizzard_maze
 {
     std::vector<blizzard> blizzards;
@@ -25,7 +47,7 @@ struct blizzard_maze
 
     void create_maze()
     {
-        maze = std::vector<std::vector<char>>(y_size, std::vector<char>(x_size, '.'));
+        init_maze();
         for (auto b : blizzards)
         {
             if (maze[b.y][b.x] == '.')
@@ -39,6 +61,20 @@ struct blizzard_maze
             else
             {
                 maze[b.y][b.x] = '2';
+            }
+        }
+    }
+
+    void init_maze()
+    {
+        maze = std::vector<std::vector<char>>(y_size, std::vector<char>(x_size, '#'));
+        maze[0][1] = '.';
+        maze[maze.size() - 1][maze[0].size() - 2] = '.';
+        for (int y = 1; y < maze.size() - 1; y++)
+        {
+            for (int x = 1; x < maze[y].size() - 1; x++)
+            {
+                maze[y][x] = '.';
             }
         }
     }
@@ -116,55 +152,73 @@ public:
 
         maze_list mazes(parse_blizzards(input), maze_x_size, maze_y_size);
 
-        std::cout << "bfs" << std::endl;
-        std::cout << bfs_maze_count(mazes);
+        person_pos_3d start(1, 0, 0);
+        person_pos_3d dest(maze_x_size - 2, maze_y_size - 1, 0);
+
+        std::cout << "The fewest minutesto reach the goal is " << bfs_to_destination(mazes, start, dest).maze_index << std::endl;
     }
 
-    int bfs_maze_count(maze_list &mazes)
+    void part_two()
+    {
+        std::vector<std::string> input = this->get_input();
+        this->maze_x_size = input[0].size();
+        this->maze_y_size = input.size();
+
+        maze_list mazes(parse_blizzards(input), maze_x_size, maze_y_size);
+
+        person_pos_3d start(1, 0, 0);
+        person_pos_3d dest(maze_x_size - 2, maze_y_size - 1, 0);
+
+        dest = bfs_to_destination(mazes, start, dest);
+        start = bfs_to_destination(mazes, dest, start);
+        dest = bfs_to_destination(mazes, start, dest);
+
+        std::cout << "The fewest minutes to reach the goal, go back to the start, then reach the goal again is "
+                  << bfs_to_destination(mazes, start, dest).maze_index
+                  << std::endl;
+    }
+
+    person_pos_3d bfs_to_destination(maze_list &mazes, person_pos_3d start, person_pos_3d dest)
     {
         // person x,y & maze_index
-        std::queue<std::pair<std::pair<int, int>, int>> search;
-        std::set<std::pair<std::pair<int, int>, int>> visited_coordinates;
+        std::queue<person_pos_3d> search;
+        std::set<person_pos_3d> visited_coordinates;
 
-        search.push({{1, 0}, 0});
+        search.push(start);
         while (true)
         {
             auto current_pos = search.front();
             search.pop();
-            if (current_pos.first.first == maze_x_size - 2 && current_pos.first.second == maze_y_size - 1)
+            if (current_pos.x == dest.x && current_pos.y == dest.y)
             {
-                print_blizzard_maze(mazes.get_maze(current_pos.second), current_pos.first.first, current_pos.first.second);
-                return current_pos.second;
+                // print_blizzard_maze(mazes.get_maze(current_pos.maze_index), current_pos.x, current_pos.y);
+                return current_pos;
             }
             std::vector<std::pair<int, int>> offset = {{0, 0}, {1, 0}, {-1, 0}, {0, 1}, {0, -1}};
             for (auto &o : offset)
             {
-                int new_x = current_pos.first.first + o.first;
-                int new_y = current_pos.first.second + o.second;
+                int new_x = current_pos.x + o.first;
+                int new_y = current_pos.y + o.second;
                 if (new_x < 0 || new_x > maze_x_size - 1 || new_y < 0 || new_y > maze_y_size - 1)
                 {
                     continue;
                 }
-                if (mazes.get_maze(current_pos.second + 1).maze[new_y][new_x] == '.' &&
-                    visited_coordinates.find({{new_x, new_y}, current_pos.second + 1}) == visited_coordinates.end())
+                person_pos_3d person_new_pos(new_x, new_y, current_pos.maze_index + 1);
+                if (visited_coordinates.find(person_new_pos) == visited_coordinates.end() &&
+                    mazes.get_maze(current_pos.maze_index + 1).maze[new_y][new_x] == '.')
                 {
-                    search.push({{new_x, new_y}, current_pos.second + 1});
-                    visited_coordinates.insert({{new_x, new_y}, current_pos.second + 1});
+                    search.push(person_new_pos);
+                    visited_coordinates.insert(person_new_pos);
                 }
             }
-            if (current_pos.first == std::pair(0, -1))
-            {
-                current_pos.second++;
-                search.push(current_pos);
-            }
-            // print_blizzard_maze(mazes.get_maze(current_pos.second), current_pos.first.first, current_pos.first.second);
+            // print_blizzard_maze(mazes.get_maze(current_pos.maze_index), current_pos.x, current_pos.y);
         }
     }
 
-    void print_blizzard_maze(blizzard_maze maze, int x, int y)
+    void print_blizzard_maze(blizzard_maze maze, int player_x, int player_y)
     {
-        if (x >= 0 && y >= 0)
-            maze.maze[y][x] = 'E';
+        if (player_x >= 0 && player_y >= 0)
+            maze.maze[player_y][player_x] = 'E';
         auto m = maze.maze;
         for (int y = 0; y < m.size(); y++)
         {
@@ -177,10 +231,6 @@ public:
         std::cout << std::endl;
     }
 
-    void part_two()
-    {
-    }
-
     std::vector<blizzard> parse_blizzards(std::vector<std::string> &input)
     {
         std::vector<blizzard> blizzards;
@@ -188,7 +238,7 @@ public:
         {
             for (int x = 0; x < input[y].size(); x++)
             {
-                if (input[y][x] != '.')
+                if (input[y][x] != '.' && input[y][x] != '#')
                 {
                     blizzards.push_back(blizzard(input[y][x], x, y));
                 }
